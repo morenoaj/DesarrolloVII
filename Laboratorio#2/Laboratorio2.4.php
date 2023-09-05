67331771f9b048743728b14b736d7265b7268171
morenoaj <ajmoreno24@outlook.com>
2023-09-05 11:55:14 -0500

    1: Agregando Laboratorios completados

---
+++ Laboratorio#2/Laboratorio2.4.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 2.4</title>
+</head>
+<body>
+    <?php
+    //Creación del arreglo array("clave" => "valor")
+    $personas = array("Juan" => "Panamá",
+    "John" => "USA",
+    "Eica" => "Finlandia",
+    "Kusanagi" => "Japon");
+
+    //Recorrer todo el arreglo
+    foreach($personas as $persona => $pais){
+        print "$persona es de $pais<br>";
+    }
+    
+    //Impresión especifica
+    echo "<br>".$personas["Juan"];
+    echo "<br>".$personas["Eica"];?>
+</body>
+</html>
